5f960c0f9cad630c572aad48ea3d483bdfa36b7d
Christopher Brandt <xtopher.brandt@gmail.com>
2019-02-10 21:47:54 -0800

    2019.12 : create

--- functions/app.js
+++ functions/app.js
@@ -78,7 +78,7 @@ function welcome(conv) {
 
     conv.ask(new SimpleResponse({
         speech: `Good ${dayPartName} from Whistler! How can I help you?`,
-        text: `Good ${dayPartName} from Whistler! How can I help you?`,
+        text: `Good ${dayPartName} from Whistler! How can I help you? V2019_12`,
     }));
     
     conv.ask(new Suggestions(welcomeSuggestions));

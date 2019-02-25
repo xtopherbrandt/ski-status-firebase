435bf22b913dc1cf46267be8de3ef32f90230d6b
Christopher Brandt <xtopher.brandt@gmail.com>
2019-02-24 20:47:02 -0800

    2019.17 : Initial commit

--- functions/index.js
+++ functions/index.js
@@ -37,4 +37,4 @@ const functions = require('firebase-functions');
 //
 // The version of the function should match the git branch name
 
-exports.fulfillment_2019_16 = functions.https.onRequest(app);
+exports.fulfillment_2019_17 = functions.https.onRequest(app);
